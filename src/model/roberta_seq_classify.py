e62793dd2ef43d440ee24fc4dd905e3e044978db
quan nguyen <21521333@gm.uit.edu.vn>
2023-05-29 15:05:12 +0700

    fix

--- src/model/roberta_seq_classify.py
+++ src/model/roberta_seq_classify.py
@@ -9,17 +9,16 @@ class Roberta_Model(nn.Module):
     def __init__(self, config: Dict, num_labels: int):
      
         super(Roberta_Model, self).__init__()
-        self.num_labels = num_labels
         self.text_embedding = Text_Embedding(config)
         self.pretrained = config["model"]["pretrained"]
         roberta_config = RobertaConfig.from_pretrained(self.pretrained)
         roberta_config.hidden_size = config["attention"]["d_model"]  # Đặt kích thước tầng ẩn là 128
         roberta_config.num_labels = num_labels  # Đặt số lượng lớp
-        roberta_config.num_hidden_layers=config["attention"]["layers"]
-        roberta_config.num_attention_heads=config["attention"]["heads"]
+        roberta_config.num_hidden_layers = config["attention"]["layers"]
+        roberta_config.num_attention_heads = config["attention"]["heads"]
         roberta_config.hidden_dropout_prob = config["attention"]["dropout"]
 
-        self.classifier = RobertaForSequenceClassification(config=roberta_config)
+        self.classifier = RobertaForSequenceClassification.from_pretrained(self.pretrained, config=roberta_config)
 
     def forward(self, text: List[str], labels: Optional[torch.LongTensor] = None):
         embbed, mask = self.text_embedding(text)
