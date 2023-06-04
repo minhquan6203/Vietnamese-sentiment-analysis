ebd62632233d4d0055585f1c705c6e4ea2a8fb91
quan nguyen <21521333@gm.uit.edu.vn>
2023-06-04 19:46:24 +0700

    fix

--- src/utils/cnn.py
+++ src/utils/cnn.py
@@ -25,7 +25,6 @@ class Text_CNN(nn.Module):
         self.bn4 = nn.BatchNorm1d(32)
         self.relu4 = nn.ReLU()
                 
-
         self.relu5 = nn.ReLU()
         self.dropout = nn.Dropout(0.2)
         self.fc1 = nn.Linear(32, num_classes)
@@ -34,7 +33,6 @@ class Text_CNN(nn.Module):
         
     def forward(self, x):
         x = x.permute(0, 2, 1)
-        
         x = self.relu1(self.bn1(self.pool1(self.conv1(x))))
         x = self.relu2(self.bn2(self.pool2(self.conv2(x))))
         x = self.relu3(self.bn3(self.pool3(self.conv3(x))))
