4b4e17d24d9d51b32906ca06a83b65c88166e90a
quan nguyen <21521333@gm.uit.edu.vn>
2023-06-27 01:00:51 +0700

    fix

--- src/text_module/count_vectorizer.py
+++ src/text_module/count_vectorizer.py
@@ -8,26 +8,22 @@ class CountVectorizer(nn.Module):
         self.word_to_idx = {word: i for i, word in enumerate(vocab)}
         self.proj = nn.Linear(len(vocab), config["text_embedding"]["d_model"])
         
-    def forward(self, sentences):
+    def forward(self, input_texts):
         count_vectors = []
         
-        for sentence in sentences:
+        for input_text in input_texts:
             word_counts = torch.zeros(len(self.vocab))
             
-            for word in sentence.split():
+            for word in input_text.split():
                 word=word.lower()
                 if word in self.vocab:
-                    word_idx = self.word_to_idx[word]
-                    word_counts[word_idx] = 1
+                    word_counts[self.vocab.index(word)] += 1
                 else:
-                    # Xử lý trường hợp từ không xuất hiện trong vocab
-                    unknown_idx = self.word_to_idx.get("unknown", None)
-                    if unknown_idx is not None:
-                        word_counts[unknown_idx] = 1
-            
+                    word_counts[self.vocab.index("unknown")] += 1
             count_vectors.append(word_counts)
         
         count_vectors = torch.stack(count_vectors, dim=0)  # Xếp các word_counts thành một tensor
         count_vectors = count_vectors.to(self.proj.weight.device)  # Chuyển đổi sang cùng device với self.proj
         
         return self.proj(count_vectors).unsqueeze(1)
+
